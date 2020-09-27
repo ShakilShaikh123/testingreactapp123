b8588948af9bc931f95d115d0a3f6ce44a5ceb2a
Shakil <shakil_shaikh1@persistent.com>
2020-09-27 13:54:38 +0530

    Initial Code

---
+++ precache-manifest.5e8e2ade60d28db24619ecdddeeb4a23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369e9ce7eb497550528f4ca6c9f3128d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e678cf04b997f2a4b919",
+    "url": "./static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "dede1badcf69586d32ee",
+    "url": "./static/css/main.9a4c3cda.chunk.css"
+  },
+  {
+    "revision": "e678cf04b997f2a4b919",
+    "url": "./static/js/2.a755e6bb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "./static/js/2.a755e6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dede1badcf69586d32ee",
+    "url": "./static/js/main.ba34de4a.chunk.js"
+  },
+  {
+    "revision": "847f68c20d6c9add0bf4",
+    "url": "./static/js/runtime-main.de51306d.js"
+  },
+  {
+    "revision": "f3528d46bc1e18659c1639b7674eada1",
+    "url": "./static/media/img2.f3528d46.png"
+  }
+]);
